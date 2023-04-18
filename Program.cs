56ee370c7b77ab8046c84546ca0eeb72eba267a9
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-04-17 17:26:24 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prog_120_S23_L6_Double_Decimal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Prog_120_S23_L6_Double_Decimal
+ 
